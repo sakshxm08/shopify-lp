014d5c19be6fb6b0addf560fd2f039e060e59595
Saksham Gambhir <saksham2211gambhir@gmail.com>
2022-08-20 00:18:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function SubForm (){
+let inputEmail = document.getElementById('email');
+function SubForm(){
+  
+  if (inputEmail.value="" || !(inputEmail.value.includes('@'))){
+    alert('Please enter a valid email adress.');
+  }
+  else{
     $.ajax({
         url:'https://api.apispreadsheets.com/data/do4Q3KBo2p9PHl2Q/',
         type:'post',
@@ -10,7 +16,7 @@ function SubForm (){
           alert("There was an error :(")
         }
     });
-    const inputEmail = document.getElementById('email');
-    inputEmail.value = '';
 
+  }  
+  inputEmail.value = '';
 }
