9ab0311a01cfeeae2c53dc8648954141b3328f1f
Saksham Gambhir <saksham2211gambhir@gmail.com>
2022-08-20 15:27:27 +0530

    Add loading animation.

--- script.js
+++ script.js
@@ -1,22 +1,69 @@
-let inputEmail = document.getElementById('email');
-function SubForm(){
-  
-  if (inputEmail.value="" || !(inputEmail.value.includes('@'))){
-    alert('Please enter a valid email adress.');
-  }
-  else{
-    $.ajax({
-        url:'https://api.apispreadsheets.com/data/do4Q3KBo2p9PHl2Q/',
-        type:'post',
-        data:$("#myForm").serializeArray(),
-        success: function(){
-          alert("Email registered successfully.")
-        },
-        error: function(){
-          alert("There was an error :(")
-        }
-    });
+let inputEmail = document.getElementById("email");
+let emailBtn = document.getElementById("emailBtn");
+let spinId = document.getElementById("spinner");
+let btnText = document.getElementById("emailBtnText");
 
-  }  
-  inputEmail.value = '';
+function removeClass() {
+  spinId.className = spinId.className.replace("spin", "");
+  spinId.innerHTML = ``;
+  btnText.className = btnText.className.replace("dispNone", "");
+  inputEmail.value = "";
+}
+
+function SubForm() {
+  if (
+    (inputEmail.value !=
+      "" && inputEmail.value.includes("@") && inputEmail.value.includes("."))
+  ) {
+    spinId.className = spinId.className + " spin";
+      btnText.className = btnText.className + " dispNone";
+      spinId.innerHTML =
+        spinId.innerHTML +
+        `
+          <div class="wave"></div>
+          <div class="wave"></div>
+          <div class="wave"></div>
+          <div class="wave"></div>
+          <div class="wave"></div>
+          <div class="wave"></div>
+          <div class="wave"></div>
+          <div class="wave"></div>
+          <div class="wave"></div>
+          <div class="wave"></div>`;
+      setTimeout(removeClass, 3000);
+    setTimeout(function() {
+      $.ajax({
+        url: "https://api.apispreadsheets.com/data/do4Q3KBo2p9PHl2Q/",
+        type: "post",
+        data: $("#myForm").serializeArray(),
+        success: function () {
+          alert("Email registered successfully.");
+        },
+        error: function () {
+          alert("There was an error :(");
+        },
+      });
+    }, 2500);
+  }
+   else {
+    spinId.className = spinId.className + " spin";
+    btnText.className = btnText.className + " dispNone";
+    spinId.innerHTML =
+      spinId.innerHTML +
+      `
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>`;
+    setTimeout(removeClass, 500);
+    setTimeout(function(){
+      alert("Please enter a valid email address.");
+    }, 600);
+  }
 }
